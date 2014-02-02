344f3eb4a54bb41e7bc97144829f3cb2b5d8dc68
Dmitry Weiner <dmitry.weiner@gmail.com>
2014-02-02 20:42:38 +0700

    Fixed bug with multy threads, changed processing speed

--- java/com/example/tg/MainActivity.java
+++ java/com/example/tg/MainActivity.java
@@ -2,7 +2,6 @@ package com.example.tg;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.View;
 import android.graphics.*;
 import android.widget.*;
 import android.widget.SeekBar.OnSeekBarChangeListener;
@@ -12,23 +11,14 @@ public class MainActivity extends ActionBarActivity
         implements OnSeekBarChangeListener{
     Bitmap bmp = null;
     ImageView iv = null;
-    String tag = "TG";
+    private boolean isCalculating = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Log.i(tag, "Start!");
         iv = (ImageView) findViewById(R.id.imageView);
-        Button btn = (Button) findViewById(R.id.button);
-        btn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.i(tag, "Click");
-                new Thread(drawImage).start();
-            }
-        });
 
         SeekBar sbr = (SeekBar) findViewById(R.id.sbr);
         SeekBar sbR = (SeekBar) findViewById(R.id.sbR);
@@ -36,10 +26,33 @@ public class MainActivity extends ActionBarActivity
         sbr.setOnSeekBarChangeListener(this);
         sbR.setOnSeekBarChangeListener(this);
         sbh.setOnSeekBarChangeListener(this);
+        updateTextValues(sbr);
+        updateTextValues(sbR);
+        updateTextValues(sbh);
     }
 
     @Override
     public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+        updateTextValues(seekBar);
+    }
+
+    @Override
+    public void onStartTrackingTouch(SeekBar seekBar) {
+        if (bmp != null) {
+            bmp.eraseColor(Color.rgb(0, 0, 0));
+            iv.setImageBitmap(bmp);
+            iv.invalidate();
+        }
+    }
+
+    @Override
+    public void onStopTrackingTouch(SeekBar seekBar) {
+        if (!isCalculating) {
+            new Thread(drawImage).start();
+        }
+    }
+
+    public void updateTextValues(SeekBar seekBar) {
         String textValue = String.valueOf(seekBar.getProgress());
         switch (seekBar.getId()){
             case R.id.sbr:
@@ -55,21 +68,14 @@ public class MainActivity extends ActionBarActivity
                 tvh.setText(textValue);
                 break;
         }
-    }
 
-    @Override
-    public void onStartTrackingTouch(SeekBar seekBar) {
-    }
-
-    @Override
-    public void onStopTrackingTouch(SeekBar seekBar) {
     }
 
     @Override
     public void onWindowFocusChanged(boolean hasFocus) {
-        // TODO Auto-generated method stub
         super.onWindowFocusChanged(hasFocus);
         bmp = Bitmap.createBitmap( iv.getWidth(), iv.getHeight(), Bitmap.Config.ARGB_8888);
+        new Thread(drawImage).start();
     }
 
     Runnable drawImage = new Runnable() {
@@ -79,13 +85,11 @@ public class MainActivity extends ActionBarActivity
         @Override
         public void run() {
             if (bmp != null) {
-                bmp.eraseColor(Color.rgb(0, 0, 0));
-                Log.i(tag, "Run");
+                isCalculating = true;
                 c = new Canvas(bmp);
 
                 Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
                 paint.setColor(Color.rgb(0, 255, 0));
-                //bmp.setPixel(r.nextInt(bmp.getWidth()), r.nextInt(bmp.getWidth()),  Color.argb(255, r.nextInt(255), r.nextInt(255), r.nextInt(255)));
                 float x, y, phi = 0;
                 SeekBar sbr = (SeekBar) findViewById(R.id.sbr);
                 float r = (float) sbr.getProgress();
@@ -93,21 +97,29 @@ public class MainActivity extends ActionBarActivity
                 float Rbig = (float) sbR.getProgress();
                 SeekBar sbh = (SeekBar) findViewById(R.id.sbh);
                 float h = (float) sbh.getProgress();
-                Log.i(tag, "r = " + r + " R = " + Rbig + " h = " + h);
 
+                float x0 = 0, y0 = 0;
                 while (phi <= 1000) {
                     x = (Rbig - r) * (float) Math.cos(phi) + h * (float) Math.cos((Rbig - r) / r * phi);
                     y = (Rbig - r) * (float) Math.sin(phi) - h *  (float) Math.sin((Rbig - r) / r * phi);
+                    if (phi == 0) {
+                        x0 = x;
+                        y0 = y;
+                    } else {
+                        if (x == x0 && y == y0) {
+                            break;
+                        }
+                    }
                     c.drawRect(bmp.getWidth()/2 + x, bmp.getHeight()/2 - y, bmp.getWidth()/2 + x + 1, bmp.getHeight()/2 - y + 1, paint);
-                    phi += 0.01;
+                    phi += 0.05;
                 }
 
                 MainActivity.this.runOnUiThread(new Runnable() {
                     public void run() {
-                        Log.i(tag, "runOnUiThread");
                         ImageView iv = (ImageView) findViewById(R.id.imageView);
                         iv.setImageBitmap(bmp);
                         iv.invalidate();
+                        isCalculating = false;
                     }
                 });
             }
